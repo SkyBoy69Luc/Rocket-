7af726c379e3ce888607acbc031c34e32979f7d5
B533P-15\533 <luc.plante.guitare.skyboy69@gmail.com>
2019-08-20 11:54:21 -0400

    Version final pret pour la vente

---
+++ Rocket/Assets/Rocket.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rdb;
+    public int SPEEDFACTOR = 6000;
+    public int ORIGINE = 1;
+
+	// Use this for initialization
+	void Start () {
+        rdb = gameObject.GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetAxis("Jump") != 0)
+        {
+            rdb.AddRelativeForce(new Vector3(Input.GetAxis("Jump") * SPEEDFACTOR * Time.deltaTime, 0,0));
+        }
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            rdb.AddForceAtPosition(new Vector3(Input.GetAxis("Horizontal") * SPEEDFACTOR * Time.deltaTime, 0, 0), new Vector3(0, ORIGINE, 0));
+        }
+
+    }
+}
